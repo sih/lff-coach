8fead6707fb8bec34d42c52f5ec93ecf3cd08bd3
sih <sid@datasoc.ltd>
2023-09-02 08:51:12 +0100

    Update to Java 19 and add 2023 brochure

--- src/test/java/eu/waldonia/lffcoach/processing/LFFProcessorTest.java
+++ src/test/java/eu/waldonia/lffcoach/processing/LFFProcessorTest.java
@@ -1,26 +1,29 @@
 package eu.waldonia.lffcoach.processing;
 
-import static org.junit.Assert.assertNotNull;
 
 import eu.waldonia.lffcoach.model.Screening;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /** @author sih */
 public class LFFProcessorTest {
   private static final String LFF_2016_PDF = "./src/test/resources/lff-2016.pdf";
   private static final String LFF_2017_PDF = "./src/test/resources/lff-2017.pdf";
   private static final String LFF_2021_PDF = "./src/test/resources/lff-2021.pdf";
+  private static final String LFF_2023_PDF = "./src/test/resources/lff-2023.pdf";
 
   private LFFProcessor lffProcessor;
   private byte[] content;
 
-  @Before
-  public void setUp() throws IOException {
+  @BeforeEach
+  void setUp() throws IOException {
     content = Files.readAllBytes(Paths.get(LFF_2021_PDF));
     lffProcessor = new LFFProcessor(content);
   }
@@ -30,11 +33,11 @@ public class LFFProcessorTest {
 
     List<Screening> screenings = lffProcessor.process();
     for (Screening s : screenings) {
-      assertNotNull(s);
-      assertNotNull(s.getCinema());
-      assertNotNull(s.getFilm());
-      assertNotNull(s.getVenueDate());
-      assertNotNull(s.getVenueTime());
+      assertThat(s).isNotNull();
+      assertThat(s.getCinema()).isNotNull();
+      assertThat(s.getFilm()).isNotNull();
+      assertThat(s.getVenueDate()).isNotNull();
+      assertThat(s.getVenueTime()).isNotNull();
     }
   }
 }
